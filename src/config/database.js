78e70a0dd7d9cfbe331b3799730174c827a4bf02
madhav.rajendra <madhav.rajendra@i-exceed.com>
2025-01-21 21:04:14 +0530

    completed the backend1

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://root:root@namastedev.ivzqt.mongodb.net/devTinder");
+    await mongoose.connect("");
 }
 
 module.exports = connectDB;
